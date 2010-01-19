ec55734f391192fb525647038ee6126187780013
Renan Leme <info@example.com>
2010-01-19 13:41:20 +0000

    initial import
    git-svn-id: http://svn.projects.ez.no/ez_publish_br@1 ad0b6411-347f-464f-b6ad-99c615937fdf

---
+++ trunk/extension/ezmultiupload/translations/por-BR/translation.ts
@@ -0,0 +1,56 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>extension/ezmultiupload</name>
+    <message>
+        <source>Multiupload</source>
+        <translation>Multi-upload</translation>
+    </message>
+    <message>
+        <source>The files are uploaded to</source>
+        <translation>Os arquivos serão enviados para</translation>
+    </message>
+    <message>
+        <source>Select files</source>
+        <translation>Selecionar arquivos</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Erro</translation>
+    </message>
+    <message>
+        <source>Starting...</source>
+        <translation>Iniciando...</translation>
+    </message>
+    <message>
+        <source>Thumbnail created.</source>
+        <translation>Miniatura criada.</translation>
+    </message>
+    <message>
+        <source>All files received.</source>
+        <translation>Todos os arquivos foram recebidos.</translation>
+    </message>
+    <message>
+        <source>Unable to load Flash content. You can download the latest version of Flash Player from the</source>
+        <translation>Não foi possível carregar conteúdo em Flash. Você pode baixar a versão mais recente do Flash Player de</translation>
+    </message>
+    <message>
+        <source>Adobe Flash Player Download Center</source>
+        <translation>Centro de downloads do Adobe Flash Player</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <source>Upload canceled.</source>
+        <translation>Envio cancelado</translation>
+    </message>
+</context>
+<context>
+    <name>design/admin/popupmenu</name>
+    <message>
+        <source>Upload multiple files</source>
+        <translation>Enviar múltiplos arquivos</translation>
+    </message>
+</context>
+</TS>
---
+++ trunk/extension/regexpline/translations/por-BR/translation.ts
@@ -0,0 +1,143 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>extension/regexpline/datatype</name>
+    <message>
+        <source>Regular Expression Text</source>
+        <comment>Datatype name</comment>
+        <translation>Linha de texto com Regex</translation>
+    </message>
+    <message>
+        <source>Your input did not meet the requirements.</source>
+        <translation>Sua entrada não atende aos requisitos.</translation>
+    </message>
+    <message>
+        <source>This is a required field.</source>
+        <translation>Campo obrigatório.</translation>
+    </message>
+    <message>
+        <source>You need at least one regular expression or selected preset</source>
+        <translation>Você precisa de pelo menos uma expressão regular ou selecionar uma pré-definida</translation>
+    </message>
+    <message>
+        <source>The regular expression &quot;%1&quot; is invalid</source>
+        <translation>A expressão regular &quot;%1&quot; é inválida</translation>
+    </message>
+</context>
+<context>
+    <name>extension/regexpline/design/standard/class/datatype/edit</name>
+    <message>
+        <source>To allow all input:</source>
+        <translation>Para permitir tudo:</translation>
+    </message>
+    <message>
+        <source>Presets</source>
+        <translation>Pré-definidas</translation>
+    </message>
+    <message>
+        <source>Help text for users</source>
+        <translation>Texto de ajuda para os usuários</translation>
+    </message>
+    <message>
+        <source>No subpatterns defined. Using the complete expression.</source>
+        <translation>Nenhum subpadrão definido. Utilizando a expressão completa.</translation>
+    </message>
+    <message>
+        <source>Object name pattern</source>
+        <translation>Padrão para nome do objeto</translation>
+    </message>
+    <message>
+        <source>This field allows you to structure the object naming pattern for this attribute. To use a subpattern of your regular expression, place its number (visible in the list below) in a tag-like notation, e.g. &amp;lt;1&amp;gt; to use the first subpattern.</source>
+        <translation>Este campo permite estruturar o padrão para nome do objeto para este atributo. Para usar um subpadrão de sua expressão regular, coloque o seu número (visível na lista abaixo) dentro de uma tag, exemplo: &amp;lt;1&amp;gt; para usar o primeiro subpadrão.</translation>
+    </message>
+    <message>
+        <source>These are the available subpatterns:</source>
+        <translation>Subpadrões disponíveis:</translation>
+    </message>
+    <message>
+        <source>Expression</source>
+        <translation>Regex</translation>
+    </message>
+    <message>
+        <source>Error message</source>
+        <translation>Mensagem de erro</translation>
+    </message>
+    <message>
+        <source>Regular expression(s) (Perl-compatible)</source>
+        <translation>Regex (Compatível com Perl)</translation>
+    </message>
+    <message>
+        <source>Add regular expression</source>
+        <translation>Adicionar expressão regular</translation>
+    </message>
+    <message>
+        <source>Remove regular expression(s)</source>
+        <translation>Remover expressões selecionadas</translation>
+    </message>
+    <message>
+        <source>Display as</source>
+        <translation>Mostrar como</translation>
+    </message>
+    <message>
+        <source>Single text line</source>
+        <translation>Linha de texto</translation>
+    </message>
+    <message>
+        <source>Text area (Text block)</source>
+        <translation>Bloco de texto</translation>
+    </message>
+    <message>
+        <source>Regexp / Preset</source>
+        <translation>Regex / Pré-definida</translation>
+    </message>
+    <message>
+        <source>Negate</source>
+        <translation>Negar</translation>
+    </message>
+    <message>
+        <source>The following errors occured:</source>
+        <translation>Os seguintes erros ocorreram:</translation>
+    </message>
+</context>
+<context>
+    <name>extension/regexpline/design/standard/class/datatype/view</name>
+    <message>
+        <source>Regular expression</source>
+        <translation>Expressão regular</translation>
+    </message>
+    <message>
+        <source>Help text</source>
+        <translation>Texto de ajuda</translation>
+    </message>
+    <message>
+        <source>Object name pattern</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>No pattern supplied. Using the complete expression.</source>
+        <translation>Nenhum subpadrão definido. Utilizando a expressão completa.</translation>
+    </message>
+    <message>
+        <source>Display type</source>
+        <translation>Mostrar como</translation>
+    </message>
+    <message>
+        <source>Identifier</source>
+        <translation>Identificador</translation>
+    </message>
+    <message>
+        <source>Negated</source>
+        <translation>Negar</translation>
+    </message>
+    <message>
+        <source>Error message</source>
+        <translation>Mensagem de erro</translation>
+    </message>
+</context>
+<context>
+    <name>extension/regexpline/design/standard/content/datatype/edit</name>
+    <message>
+        <source>Help text</source>
+        <translation>Texto de ajuda</translation>
+    </message>
+</context>
+</TS>
